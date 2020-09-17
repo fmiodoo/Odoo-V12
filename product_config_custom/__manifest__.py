d4384a0c2ad6c53124a1e4b99495f785bc7b218f
Keyur Gajjar <kga@odoo.com>
2020-09-17 10:49:05 -0700

    [ADD] Pending Manifest File

---
+++ product_config_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Customized Product Configurator",
+    'version': '1.0',
+    'depends': ['sale_product_configurator'],
+    'author': 'Odoo Inc',
+    'license': 'OEEL-1',
+    'mainainer': 'Odoo Inc',
+    'category': 'Custom',
+    'description': """
+Customized Product Configurator
+================================
+
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/product_views.xml',
+    ],
+}
