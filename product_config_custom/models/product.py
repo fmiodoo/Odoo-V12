9b10455f4bf656a77a769ea0a1fa15564ff28b75
Keyur Gajjar <kga@odoo.com>
2020-10-06 16:38:16 -0700

    [ADD] added attribute groups so that when generating code it gets combined in groups then conatenated with '-'. - Added atttribute group menu entry. - On attribute added boolean which determines that values itself will be used in the code or not. - Other improvements.

--- product_config_custom/models/product.py
+++ product_config_custom/models/product.py
@@ -4,8 +4,26 @@
 from odoo import api, fields, models
 
 
+class ProductAttributeGroup(models.Model):
+    _name = 'product.attribute.group'
+    _order = 'sequence'
+    _description = """
+    Object to handle sequence of attribute values and grouping when generating default code.
+    """
+
+    name = fields.Char(string="Group Name")
+    sequence = fields.Integer()
+    attribute_ids = fields.One2many(comodel_name='product.attribute', inverse_name='group_id', string='Attributes')
+
+
+class ProductAttribute(models.Model):
+    _inherit = 'product.attribute'
+
+    group_id = fields.Many2one(comodel_name='product.attribute.group', string="Attribute Group")
+    value_is_code = fields.Boolean()
+
 class PTAttributeValue(models.Model):
-    _inherit = 'product.template.attribute.value'
+    _inherit = 'product.attribute.value'
 
     code = fields.Char(copy=False)
 
@@ -15,8 +33,13 @@ class ProductTemplate(models.Model):
     
     def _create_product_variant(self, combination, log_warning=False):
         product = super(ProductTemplate, self)._create_product_variant(combination, log_warning=log_warning)
-        attribute_value_codes = product.product_template_attribute_value_ids.mapped('code')
-        product.default_code = '-'.join([code for code in attribute_value_codes if code])
+        groups = []
+        pavs = product.product_template_attribute_value_ids.mapped('product_attribute_value_id')
+        product_attributes = pavs.mapped('attribute_id')
+        for attr_group in product_attributes.mapped('group_id'):
+            codes = [(pav.name if pav.attribute_id.value_is_code else pav.code) for pav in pavs.filtered(lambda v: v.attribute_id in attr_group.attribute_ids)]
+            groups.append(''.join([code for code in codes if code]))
+        product.default_code = '-'.join(groups)
         return product
 
 
