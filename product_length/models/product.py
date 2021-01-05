7cdad7fc0cce2e4da1a1cced3d45e5ea5a89fe91
Johnny Sanchez <jsz@odoo.com>
2021-01-05 15:05:11 -0800

    [FIX] Fixed product and sale order line models
    Removed the field for length on product.product so that only sale order
    line will have it and use it for price calculation.
    
    Removed product.product inherited model as it was no longer necessary.
    
    Modified the price calculation on sale order line to correctly
    calculate price for cable lengths less than 1.0.
    
    Fixed all previous desired_length references in both models and views to
    the new length field.

--- product_length/models/product.py
+++ product_length/models/product.py
@@ -17,34 +17,3 @@ class ProductTemplate(models.Model):
     def _check_length_multiplier(self):
         if self.length_multiplier <= 0.0:
             raise ValidationError("Field Length Cost Multiplier must be a positive value.")
-
-
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-
-    desired_length = fields.Float(string="Desired Length",
-                                  default=1.0)
-    name = fields.Char(string="Name",
-                       compute="_compute_product_name")
-    x_studio_catalog_ = fields.Char(string="Catalog #",
-                                    compute="_compute_product_name")
-
-    @api.one
-    @api.constrains("desired_length")
-    def _check_desired_length(self):
-        if self.desired_length <= 0.0:
-            raise ValidationError("Field Desired Length must be a positive value.")
-
-    @api.depends("desired_length", "product_tmpl_id")
-    def _compute_product_name(self):
-        for product in self:
-            if product.is_cable_product:
-                formatted_length = "%07.1f" % product.desired_length + product.uom_id.name
-                product.name = product.product_tmpl_id.name + "-" + formatted_length
-
-                if product.x_studio_catalog_:
-                    product.x_studio_catalog_ = product.product_tmpl_id.x_studio_catalog_ + "-" + formatted_length
-            else:
-                product.name = product.product_tmpl_id.name
-                if product.x_studio_catalog_:
-                    product.x_studio_catalog_ = product.product_tmpl_id.x_studio_catalog_
--- product_length/models/sale_order_line.py
+++ product_length/models/sale_order_line.py
@@ -6,28 +6,40 @@ from odoo.exceptions import ValidationError
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    desired_length = fields.Float(string="Length",
-                                  related="product_id.desired_length",
-                                  readonly=False)
+    length = fields.Float(string="Length",
+                          default=1.0,
+                          readonly=False)
 
     name = fields.Text(string="Description",
                        compute="_compute_name")
 
-    @api.depends('product_id.name')
+    @api.one
+    @api.constrains("length")
+    def _check_length(self):
+        if self.length <= 0.0:
+            raise ValidationError("Field Length must be a positive value.")
+
+    @api.depends('length')
     def _compute_name(self):
         for line in self:
             if line.product_id.is_cable_product:
-                line.name = line.product_id.name
+                formatted_length = "%07.2f" % line.length + line.product_id.uom_id.name
+                line.name = line.product_id.name + "-" + formatted_length
             else:
                 line.name = line.product_id.get_product_multiline_description_sale()
 
-    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'product_id.length_multiplier', 'product_id.desired_length')
+    @api.depends("product_uom_qty", "discount", "price_unit", "tax_id", "product_id.length_multiplier", "length")
     def _compute_amount(self):
         """
-        Compute the amounts of the SO line.
+        Compute the amounts of the SO line.  If a line contains a cable product, the price will be computed based on
+        the length input in the order line against the 1 UoM base sale price of the product.
         """
         super(SaleOrderLine, self)._compute_amount()
 
         for line in self:
-            extra_length = line.product_id.desired_length - 1.0
-            line.price_subtotal += extra_length * line.product_id.length_multiplier
+            if line.product_id.is_cable_product:
+                if line.length < 1.0:
+                    line.price_subtotal *= line.length
+                elif line.length > 1.0:
+                    extra_length = line.length - 1.0
+                    line.price_subtotal += extra_length * line.product_id.length_multiplier
