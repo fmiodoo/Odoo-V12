e0052d363c099eb7570798062bb7a73b254c0898
Johnny Sanchez <jsz@odoo.com>
2021-04-05 16:56:19 -0700

    [FIX] Modified _compute_amount to add rounding to length price calculation.

--- fibermountain_product_length/models/sale_order_line.py
+++ fibermountain_product_length/models/sale_order_line.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api, _
+from odoo.tools import float_round
 from odoo.exceptions import ValidationError
 
 
@@ -37,26 +38,33 @@ class SaleOrderLine(models.Model):
         Compute the amounts of the SO line.  If a line contains a cable product, the price will be computed based on
         the length input in the order line against the 1 UoM base sale price of the product.
         """
-        super(SaleOrderLine, self)._compute_amount()
-
         for line in self.filtered(lambda l: l.product_id.is_cable_product):
             new_price = 0
             new_unit_price = line.product_id.list_price
+
             if line.length < 1.0:
-                new_unit_price *= line.length
+                # Only round first calculation or double rounding will occur
+                new_unit_price = float_round(new_unit_price * line.length, 2)
                 new_price = new_unit_price * (1 - (line.discount or 0.0) / 100)
+
             elif line.length > 1.0:
                 extra_length = line.length - 1.0
-                new_unit_price += (extra_length * line.product_id.length_multiplier)
+                # only round first calculation or double rounding will occur
+                new_unit_price += float_round(extra_length * line.product_id.length_multiplier, 2)
                 new_price = new_unit_price * (1 - (line.discount or 0.0) / 100)
+
             else:
-                new_price = new_unit_price * (1 - (line.discount or 0.0) / 100)
+                new_price = float_round(new_unit_price * (1 - (line.discount or 0.0) / 100), 2)
 
             taxes = line.tax_id.compute_all(new_price, line.order_id.currency_id, line.product_uom_qty,
                                             product=line.product_id, partner=line.order_id.partner_shipping_id)
+
             line.update({
                 'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
                 'price_unit': new_unit_price
             })
+
+            if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
+                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
