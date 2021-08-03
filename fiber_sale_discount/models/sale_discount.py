3fc46f97c0ff25c2ff84cd4454190e86b9f24da1
hip-odoo <hip@tinyerp.com>
2021-08-03 17:19:17 +0530

    [FIX] Code optimization

--- fiber_sale_discount/models/sale_discount.py
+++ fiber_sale_discount/models/sale_discount.py
@@ -17,17 +17,11 @@ class SaleOrder(models.Model):
 
     confirm_so = fields.Boolean('Allow to confirm sale order', default=True, compute='_compute_confirm_so', store=True, readonly=True)
     state = fields.Selection(selection_add=[('wait', 'Waiting for Approval'), ('approved', 'SO is Approved'), ('rejected', 'SO is Rejected')])
-    show_authorization = fields.Boolean(string='Authorized?', default=False, compute='_compute_show_authorization')
-    login_user = fields.Many2one('res.users', string='Logged in user', compute='_compute_login_user')
+    show_authorization = fields.Boolean(string='Authorized?', compute='_compute_show_authorization')
 
-    def _compute_login_user(self):
-        for order in self:
-            order.login_user = self.env.user
-
-    @api.depends('login_user')
     def _compute_show_authorization(self):
         for order in self:
-            if self.login_user in order.user_id.approver_ids:
+            if self.env.user in order.user_id.approver_ids:
                 order.show_authorization = True
             else:
                 order.show_authorization = False
