429aa7e8a623470f8c613873d3415750a2b9c322
aal-odoo <aal@odoo.com>
2023-03-16 16:47:56 -0700

    FIX sale order rounding for compute_amount function

---
+++ sale_fiber/model/order.py
@@ -0,0 +1,21 @@
+from odoo import api, models, fields
+from odoo.tools import float_compare, float_round
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        for line in self:
+            ##### modified code
+            price = float_round(line.price_unit * (1 - (line.discount or 0.0) / 100.0), 2)
+            ##### end modified code
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
